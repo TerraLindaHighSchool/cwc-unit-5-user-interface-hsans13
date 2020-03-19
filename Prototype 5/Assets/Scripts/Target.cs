2c013ebe080917cb3b654b89d478756ccf2a9c36
Hollis Sansing <hs00827@srcs.org>
2020-03-18 17:58:27 -0700

    DId some work on unit 5
    I keep getting notifications about errors through unity, but it doesn't show up in the code, so I dont know what's wrong

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -10,8 +10,8 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6; 
-
+    private float ySpawnPos = -6;
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
